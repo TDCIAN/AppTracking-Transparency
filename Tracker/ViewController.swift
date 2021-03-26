3c8a6d1135c03e873d7f2bfb111e9011cd822189
KJM <tdcian@hexlant.com>
2021-03-26 10:15:06 +0900

    AppTrackingTransparency import

--- Tracker/ViewController.swift
+++ Tracker/ViewController.swift
@@ -4,16 +4,38 @@
 //
 //  Created by hexlant_01 on 2021/03/26.
 //
-
+import AppTrackingTransparency
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        view.addSubview(button)
+        button.center = view.center
+        button.setTitle("Get Permission", for: .normal)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        button.setTitleColor(.systemRed, for: .normal)
     }
 
+    @objc func didTapButton() {
+        ATTrackingManager.requestTrackingAuthorization { status in
+            switch status {
+            case .notDetermined:
+                break
+            case .restricted:
+                break
+            case .denied:
+                break
+            case .authorized:
+                break
+            @unknown default:
+                break
+            }
+        }
+    }
 
 }
 
